bf3a6c42539c373c9056da33063d08a969805254
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-24 15:34:21 +0900

    Initial Commit

---
+++ sasamiCheese80/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sasamiCheese80
+//
+//  Created by sakaguchi on 2024/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sasamiCheese80/sasamiCheese80App.swift
@@ -0,0 +1,17 @@
+//
+//  sasamiCheese80App.swift
+//  sasamiCheese80
+//
+//  Created by sakaguchi on 2024/06/24.
+//
+
+import SwiftUI
+
+@main
+struct sasamiCheese80App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
